f5ca901d7038619b37be538c3b7542fcb1512142
Atreya Shankar <shankar.atreya@gmail.com>
2019-12-21 15:47:20 +0100

    *shankar* conflict resolution between develop_argumentation and master!

--- src/init.sh
+++ src/init.sh
@@ -7,12 +7,12 @@ if [ $ans == "y" ]; then
     cp ../hooks/pre-commit.sample ../.git/hooks/pre-commit
 fi
 
-read -rep "download and deply UNSC/USElection data? (y/n): " ans
+read -rep "download and deploy UNSC/USElection data? (y/n): " ans
 if [ $ans == "y" ]; then
     # get lfw-faces data
     cd ./data
     wget -O ./USElectionDebates/ElecDeb60To16.zip https://github.com/ElecDeb60To16/Dataset/raw/master/ElecDeb60To16.zip
-    unzip -o ./USElectionDebates/ElecDeb60To16.zip -d ./USElection/
+    unzip -o ./USElectionDebates/ElecDeb60To16.zip -d ./USElectionDebates/
     wget -O ./UNSC/docs.RData https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KGVSYH/G2DENH
     wget -O ./UNSC/docs_meta.RData https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KGVSYH/KHJOUV
     cd ..
