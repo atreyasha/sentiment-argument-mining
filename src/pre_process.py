ea0c057982357d7a8289269cd5d03e33fdc3e5a9
Atreya Shankar <shankar.atreya@gmail.com>
2020-01-18 19:40:01 +0100

    *shankar* test script

---
+++ src/pre_process.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import csv
+import nltk
+import codecs
+import argparse
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from obj.arg_metav_formatter import *
+
+def read_us_election_corpus():
+    """
+    Function to read US election debate text and annotations. Conducts sanity
+    check on text vs. annotations to ensure data makes sense.
+
+    Returns:
+        (list,list): raw text and annotated argument candidates respectively
+    """
+    # read raw text into memory by sorting first through indices
+    files_raw = glob("./data/USElectionDebates/*.txt")
+    indices = [int(re.sub(r".*\/(\d+)_(\d+).*","\g<1>",File))
+               for File in files_raw]
+    files_raw = [f for i,f in sorted(zip(indices,files_raw))]
+    files_ann = glob("./data/USElectionDebates/*.ann")
+    indices = [int(re.sub(r".*\/(\d+)_(\d+).*","\g<1>",File))
+               for File in files_ann]
+    files_ann = [f for i,f in sorted(zip(indices,files_ann))]
+    # read raw text into pythonic list
+    raw = []
+    ann = []
+    print("reading raw corpus files")
+    for File in tqdm(files_raw):
+        with codecs.open(File,"r",encoding="utf-8") as f:
+            raw.append(f.read())
+    # read annotations and append to merged list
+    print("reading corpus annotations")
+    for i,File in tqdm(enumerate(files_ann),total=len(files_ann)):
+        with codecs.open(File,"r",encoding="utf-8") as f:
+            ann_data = f.readlines()
+        for annotation in ann_data:
+            hold = annotation.replace("\n","").split("\t")
+            split = re.split(r"\s+|\;",hold[1])
+            spans = [int(el) for el in split[1:]]
+            spans = list(zip(*(iter(spans),)*2))
+            # check that individual subsets match overall string
+            assert " ".join(raw[i][pair[0]:pair[1]]
+                            for pair in spans) == hold[2]
+            ann.append([i,hold[0],split[0],spans,hold[2]])
+    return raw,ann
+
+def char_tag(corpus,spaces=False):
+    """
+    Function to convert raw US election debate text
+    and annotations into tagged character sequence
+
+    Args:
+        corpus (list,list): output of "read_us_election_corpus"
+        spaces (bool): True if spaces should be marked, False if
+        they should be marked same as span
+
+    Returns:
+        (list): each component contains annotated characters
+    """
+    # create empty list
+    tagged = []
+    # tag basic cases
+    print("tagging void argument cases")
+    for i,raw in enumerate(tqdm(corpus[0])):
+        char_raw = list(raw)
+        for j,char in enumerate(char_raw):
+            if spaces:
+                if char not in [" ","\n","\r"]:
+                    char_raw[j] = "N"
+                elif char == " ":
+                    char_raw[j] = "S"
+            else:
+                if char not in ["\n","\r"]:
+                    char_raw[j] = "N"
+        tagged.append(char_raw)
+    # tag claims and premises
+    print("tagging claims and premises")
+    for i in tqdm(range(len(tagged))):
+        for annotation in corpus[1]:
+            if annotation[0] == i:
+                ann_type = annotation[2]
+                spans = annotation[3]
+                for z,pair in enumerate(spans):
+                    for j in range(pair[0],pair[1]):
+                        char = tagged[i][j]
+                        if spaces:
+                            if char not in ["S","\n","\r"]:
+                                if ann_type == "Claim":
+                                    tagged[i][j] = "C"
+                                elif ann_type == "Premise":
+                                    tagged[i][j] = "P"
+                        else:
+                            if char not in ["\n","\r"]:
+                                if ann_type == "Claim":
+                                    tagged[i][j] = "C"
+                                elif ann_type == "Premise":
+                                    tagged[i][j] = "P"
+    # join and return final tag
+    print("returning final object")
+    return ["".join(char for char in segment) for segment in tqdm(tagged)]
+
+def flatten(char_sequences):
+    """
+    Function to split and flatten character sequences
+
+    Args:
+        char_sequences (list): character sequences
+
+    Returns:
+        (list): flattened arguments character sequences
+    """
+    flat = []
+    for speech in char_sequences:
+        split = re.split(r"\r\n|\n|\r|\n\r",speech)
+        for segment in split:
+            if segment != "":
+                flat.append(segment)
+    return flat
+
+def correct_periods(flat_text,flat_ann,spaces=False):
+    """
+    Function to add spaces where incorrect periods are present
+
+    Args:
+        flat_text (list): character sequences for raw rext from "flatten"
+        flat_ann (list): character sequences for tagged text from "flatten"
+        spaces (bool): True if spaces are to be annotated, False if not
+
+    Returns:
+        (list,list): corrected versions of flat_text and flat_ann
+    """
+    for i in range(len(flat_text)):
+        run = True
+        while run:
+            match = re.search(r"[a-z]\.[A-Z]",flat_text[i])
+            if match == None:
+                run = False
+            else:
+                flat_text[i] = (flat_text[i][:match.end()-1] +
+                                " " + flat_text[i][match.end()-1:])
+                if spaces:
+                    flat_ann[i] = (flat_ann[i][:match.end()-1] +
+                                "S" + flat_ann[i][match.end()-1:])
+                else:
+                    forward_ann = flat_ann[i][match.end()-1]
+                    flat_ann[i] = (flat_ann[i][:match.end()-1] +
+                                forward_ann + flat_ann[i][match.end()-1:])
+    return flat_text,flat_ann
+
+def tokenize(flat_text,flat_ann):
+    """
+    Function to prune and tokenize corpus
+
+    Args:
+        flat_text (list): character sequences for raw rext from "flatten"
+        flat_ann (list): character sequences for tagged text from "flatten"
+
+    Returns:
+        (list): pruned list of tokens and associated annotations
+    """
+    # first loop to zip results and check for equal length initial tokens
+    split_combined = []
+    print("tokenizing and performing sanity checks")
+    try:
+        nltk.word_tokenize("testing.")
+    except LookupError:
+        nltk.download('punkt')
+    for i in tqdm(range(len(flat_text))):
+        split_text = [nltk.word_tokenize(el)
+                    for el in flat_text[i].split(" ")]
+        split_ann = [(el,Counter(el)) for el in flat_ann[i].split("S")]
+        assert len(split_ann) == len(split_text)
+        split_combined.append(list(zip(split_text,split_ann)))
+    # prune tokens to ensure proper splits
+    split_combined_pruned = []
+    # next loop to check and combine results
+    print("tagging tokens")
+    for segment in tqdm(split_combined):
+        temp = []
+        for token_set in segment:
+            if len(token_set[0]) > 0:
+                most_common = token_set[1][1].most_common()[0][0]
+                if len("".join(token_set[0])) == len(token_set[1][0]):
+                    if len(token_set[1][1]) == 1:
+                        for token in token_set[0]:
+                            temp.append([token,most_common])
+                    else:
+                        count = 0
+                        for token in token_set[0]:
+                            tag = Counter(token_set[1][0]
+                                        [count:(count+len(token))])
+                            most_common_local = tag.most_common()[0][0]
+                            temp.append([token,most_common_local])
+                            count += len(token)
+                else:
+                    for token in token_set[0]:
+                        temp.append([token,most_common])
+        split_combined_pruned.append(temp)
+    return split_combined_pruned
+
+def write_to_file(data,subtype,directory,name,header):
+    """
+    Function to write parsed text to CoNLL file format
+
+    Args:
+        data (list): output from corpus2char/corpus2tokens
+        subtype (str): either char (characters) or tokens
+        directory (str): directory to store files
+        name (str): name of file
+        header (str): header of CoNLL stored files
+    """
+    if subtype == "char":
+        with open(directory+name,"w") as f:
+            writer = csv.writer(f,delimiter="\t")
+            writer.writerow(header)
+            writer.writerow([])
+            for i in range(len(data[0])):
+                writer.writerow([data[0][i],data[1][i]])
+                writer.writerow([])
+    elif subtype == "tokens":
+        with open(directory+name,"w") as f:
+            writer = csv.writer(f,delimiter="\t")
+            writer.writerow(header)
+            writer.writerow([])
+            for token_set in data:
+                for token,tag in token_set:
+                    writer.writerow([token,tag])
+                writer.writerow([])
+
+def corpus2char(directory="./data/pre-processed/task_1/char/"):
+    """
+    Function to convert US election corpus to character representation
+    and save in CoNLL format
+
+    Args:
+        directory (str): base file directory on which to store output
+    """
+    if not directory.endswith("/"):
+        directory += "/"
+    corpus = read_us_election_corpus()
+    tagged = char_tag(corpus,spaces=False)
+    flat_text = flatten(corpus[0])
+    flat_ann = flatten(tagged)
+    assert len(flat_text) == len(flat_ann)
+    flat_text,flat_ann = correct_periods(flat_text,flat_ann,spaces=False)
+    (flat_text_train,flat_text_test,
+     flat_ann_train,flat_ann_test) = train_test_split(flat_text,
+                                                      flat_ann,test_size=0.33,
+                                                      random_state=42)
+    header = ["-DOCSTART-","-char_seq-",
+              "-char_seq_annotated->[P=premise,C=claim,N=None]-"]
+    write_to_file([flat_text_train,flat_ann_train],"char",
+                  directory,"train.txt",header)
+    write_to_file([flat_text_test,flat_ann_test],"char",
+                  directory,"test.txt",header)
+
+def corpus2tokens(directory="./data/pre-processed/task_1/tokens/"):
+    """
+    Function to convert US election corpus to token representation
+    and save in CoNLL format
+
+    Args:
+        directory (str): base file directory on which to store output
+    """
+    if not directory.endswith("/"):
+        directory += "/"
+    corpus = read_us_election_corpus()
+    tagged = char_tag(corpus,spaces=True)
+    flat_text = flatten(corpus[0])
+    flat_ann = flatten(tagged)
+    assert len(flat_text) == len(flat_ann)
+    flat_text,flat_ann = correct_periods(flat_text,flat_ann,spaces=True)
+    split_combined = tokenize(flat_text,flat_ann)
+    train, test = train_test_split(split_combined,test_size=0.33,
+                                   random_state=42)
+    header = ["-DOCSTART-","-token-",
+              "-annotation->[P=premise,C=claim,N=None]-"]
+    write_to_file(train,"tokens",directory,"train.txt",header)
+    write_to_file(test,"tokens",directory,"test.txt",header)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=arg_metav_formatter)
+    parser.add_argument("--dtype", type=str, default="tokens",
+                        help="which type of data pre-processing;"+
+                        " either tokens, char or both")
+    args = parser.parse_args()
+    assert args.dtype in ["tokens","char","both"]
+    if args.dtype == "tokens":
+        corpus2tokens()
+    elif args.dtype == "char":
+        corpus2char()
+    elif args.dtype == "both":
+        print("processing tokens")
+        corpus2tokens()
+        print("processing characters")
+        corpus2char()
---
+++ src/train.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import math
+import datetime
+import bert
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+from obj.arg_metav_formatter import *
+import logging
+
+# read in data as string representation
+# map labels to integers, pad labels as well
+# TODO use kamalkraj's implementation for data formatting, use local keras library for model handling
+# TODO need to modify end of system to handle masking
+# TODO replace local tokenizers with those suggested in bert-tf-2
+# TODO issue of trimming/padding before or inside model
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+        Args:
+            guid: Unique id for the example.
+            text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_id, valid_ids=None, label_mask=None):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+        self.valid_ids = valid_ids
+        self.label_mask = label_mask
+
+def readfile(filename):
+    """
+    read file
+    """
+    f = open(filename)
+    data = []
+    sentence = []
+    label = []
+    for line in f:
+        if len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n":
+            if len(sentence) > 0:
+                data.append((sentence, label))
+                sentence = []
+                label = []
+            continue
+        splits = line.split('\t')
+        sentence.append(splits[0])
+        label.append(splits[-1][:-1])
+    if len(sentence) > 0:
+        data.append((sentence, label))
+        sentence = []
+        label = []
+    return data
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_labels(self):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        return readfile(input_file)
+
+
+class NerProcessor(DataProcessor):
+    """Processor for the CoNLL-2003 data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.txt")), "train")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.txt")), "test")
+
+    def get_labels(self):
+        return ["C", "P", "N", "[CLS]", "[SEP]"]
+
+    def _create_examples(self, lines, set_type):
+        examples = []
+        for i, (sentence, label) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = ' '.join(sentence)
+            text_b = None
+            label = label
+            examples.append(InputExample(
+                guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
+# TODO modify tokenizer here
+def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+    label_map = {label: i for i, label in enumerate(label_list, 1)}
+    features = []
+    for (ex_index, example) in enumerate(examples):
+        textlist = example.text_a.split(' ')
+        labellist = example.label
+        tokens = []
+        labels = []
+        valid = []
+        label_mask = []
+        for i, word in enumerate(textlist):
+            token = tokenizer.tokenize(word)
+            tokens.extend(token)
+            label_1 = labellist[i]
+            for m in range(len(token)):
+                if m == 0:
+                    labels.append(label_1)
+                    valid.append(1)
+                    label_mask.append(True)
+                else:
+                    valid.append(0)
+        if len(tokens) >= max_seq_length - 1:
+            tokens = tokens[0:(max_seq_length - 2)]
+            labels = labels[0:(max_seq_length - 2)]
+            valid = valid[0:(max_seq_length - 2)]
+            label_mask = label_mask[0:(max_seq_length - 2)]
+        ntokens = []
+        segment_ids = []
+        label_ids = []
+        ntokens.append("[CLS]")
+        segment_ids.append(0)
+        valid.insert(0, 1)
+        label_mask.insert(0, True)
+        label_ids.append(label_map["[CLS]"])
+        for i, token in enumerate(tokens):
+            ntokens.append(token)
+            segment_ids.append(0)
+            if len(labels) > i:
+                label_ids.append(label_map[labels[i]])
+        ntokens.append("[SEP]")
+        segment_ids.append(0)
+        valid.append(1)
+        label_mask.append(True)
+        label_ids.append(label_map["[SEP]"])
+        input_ids = tokenizer.convert_tokens_to_ids(ntokens)
+        input_mask = [1] * len(input_ids)
+        label_mask = [True] * len(label_ids)
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+            label_ids.append(0)
+            valid.append(1)
+            label_mask.append(False)
+        while len(label_ids) < max_seq_length:
+            label_ids.append(0)
+            label_mask.append(False)
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        assert len(label_ids) == max_seq_length
+        assert len(valid) == max_seq_length
+        assert len(label_mask) == max_seq_length
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("tokens: %s" % " ".join(
+                [str(x) for x in tokens]))
+            logger.info("input_ids: %s" %
+                        " ".join([str(x) for x in input_ids]))
+            logger.info("input_mask: %s" %
+                        " ".join([str(x) for x in input_mask]))
+            logger.info(
+                "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+        features.append(
+            InputFeatures(input_ids=input_ids,
+                          input_mask=input_mask,
+                          segment_ids=segment_ids,
+                          label_id=label_ids,
+                          valid_ids=valid,
+                          label_mask=label_mask))
+    return features
+
+import sentencepiece as spm
+spm_model = "./albert_base/30k-clean.model"
+sp = spm.SentencePieceProcessor()
+sp.load(spm_model)
+do_lower_case = True
+
+processor = NerProcessor()
+label_list = processor.get_labels()
+num_labels = len(label_list) + 1
+tokenizer = bert.albert_tokenization.FullTokenizer("./albert_base/30k-clean.vocab", do_lower_case=True,spm_model_file="./albert_base/30k-clean.model")
+
+train_examples = processor.get_train_examples("./data/pre-processed/task_1/tokens")
+label_map = {i: label for i, label in enumerate(label_list, 1)}
+train_features = convert_examples_to_features(
+    train_examples, label_list, 300, tokenizer)
+
+train_x = np.array([input_set.input_ids for input_set in train_features])
+train_y = np.array([input_set.label_id for input_set in train_features])
+
+model_name = "albert_base_v2"
+model_dir    = bert.fetch_google_albert_model(model_name, ".models")
+model_ckpt   = os.path.join(model_dir, "model.ckpt-best")
+model_params = bert.albert_params(model_dir)
+l_bert = bert.BertModelLayer.from_params(model_params, name="albert")
+
+def create_learning_rate_scheduler(max_learn_rate=5e-5,
+                                   end_learn_rate=1e-7,
+                                   warmup_epoch_count=10,
+                                   total_epoch_count=90):
+    def lr_scheduler(epoch):
+        if epoch < warmup_epoch_count:
+            res = (max_learn_rate/warmup_epoch_count) * (epoch + 1)
+        else:
+            res = max_learn_rate*math.exp(math.log(end_learn_rate/
+                                                max_learn_rate)*
+                                        (epoch-warmup_epoch_count+1)/
+                                        (total_epoch_count-
+                                        warmup_epoch_count+1))
+        return float(res)
+    learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_scheduler,
+                                                                       verbose=1)
+    return learning_rate_scheduler
+
+def create_model(l_bert,model_ckpt,max_seq_len=300):
+    """Creates a classification model."""
+    input_ids      = tf.keras.layers.Input(shape=(max_seq_len,), dtype='int32', name="input_ids")
+    output         = l_bert(input_ids)
+    print("bert shape", output.shape)
+    cls_out = tf.keras.layers.Dropout(0.5)(output)
+    logits = tf.keras.layers.Dense(units=768, activation="tanh")(cls_out)
+    logits = tf.keras.layers.Dropout(0.5)(logits)
+    logits = tf.keras.layers.Dense(units=300, activation="softmax")(logits)
+    model = tf.keras.Model(inputs=input_ids, outputs=logits)
+    model.build(input_shape=(None, max_seq_len))
+    bert.load_albert_weights(l_bert, model_ckpt)
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="acc")])
+    model.summary()
+    return model
+
+model = create_model(l_bert,model_ckpt)
+total_epoch_count=3
+
+model.fit(x=train_x,y=train_y,
+          validation_split=0.1,
+          batch_size=48,
+          shuffle=True,
+          epochs=total_epoch_count,
+          callbacks=[create_learning_rate_scheduler(max_learn_rate=1e-5,
+                                                    end_learn_rate=1e-7,
+                                                    warmup_epoch_count=20,
+                                                    total_epoch_count=total_epoch_count),
+                     tf.keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True)])
+model.save_weights('./test.h5', overwrite=True)
