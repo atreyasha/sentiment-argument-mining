5d8266c932a78ebd247ca8088005f042faebad72
Atreya Shankar <shankar.atreya@gmail.com>
2020-01-25 19:25:54 +0100

    *shankar* next step for testing

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -10,7 +10,7 @@ import tensorflow.keras.backend as K
 
 def specific_acc(label_threshold_less):
     def arg_label_acc(y_true, y_pred):
-        class_id_true = K.argmax(y_true, axis=-1)
+        class_id_true = y_true
         class_id_preds = K.argmax(y_pred, axis=-1)
         accuracy_mask = K.cast(K.less(class_id_preds,label_threshold_less),'float32')
         accuracy_mask = 1 - accuracy_mask
@@ -18,17 +18,6 @@ def specific_acc(label_threshold_less):
         class_acc = K.sum(class_acc_tensor) / K.maximum(K.sum(accuracy_mask), 1)
         return class_acc
     return arg_label_acc
-
-def single_class_accuracy(interesting_class_id):
-    def fn(y_true, y_pred):
-        class_id_true = K.argmax(y_true, axis=-1)
-        class_id_preds = K.argmax(y_pred, axis=-1)
-        # Replace class_id_preds with class_id_true for recall here
-        accuracy_mask = K.cast(K.equal(class_id_preds, interesting_class_id), 'int32')
-        class_acc_tensor = K.cast(K.equal(class_id_true, class_id_preds), 'int32') * accuracy_mask
-        class_acc = K.sum(class_acc_tensor) / K.maximum(K.sum(accuracy_mask), 1)
-        return class_acc
-    return fn
 
 def fetch_bert_layer():
     model_name = "albert_base_v2"
