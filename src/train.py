f8066834fb6a66d1447342501edef647de2ade99
Atreya Shankar <shankar.atreya@gmail.com>
2020-01-26 15:15:45 +0100

    *shankar* testing out pre-process data functions

--- src/train.py
+++ src/train.py
@@ -3,26 +3,40 @@
 
 import numpy as np
 import tensorflow as tf
+from glob import glob
 from utils.data_utils import *
 from utils.model_utils import *
 from utils.arg_metav_formatter import *
 from pre_process_us_election_debates import *
 
 def pipe_tokenized_data(max_seq_length):
-    label_map = {"<pad>":0,"[CLS]":1,"[SEP]":2,"N":3,"C":4,"P":5}
     train, test = corpus2tokens(max_seq_length=max_seq_length)
-    return train, test, label_map
+    return train, test
 
-def train(max_epochs=10,max_seq_length=128):
-    train_data, test_data, label_map = pipe_tokenized_data(max_seq_length)
-    input_ids, label_ids, _ = project_to_ids(train_data,label_map)
+def train(max_epochs=10,max_seq_length=128,
+          directory="./data/USElectionDebates/task_1/"):
+    label_map = {"<pad>":0,"[CLS]":1,"[SEP]":2,"N":3,"C":4,"P":5}
+    check = glob(directory+"*"+str(max_seq_length)+"*")
+    if len(check) != 4:
+        train_data, test_data = pipe_tokenized_data(max_seq_length)
+        train_X, train_Y, _ = project_to_ids(train_data,label_map)
+        test_X, test_Y, _ = project_to_ids(test_data,label_map)
+        np.save(directory+"train_X_"+str(max_seq_length)+".npy",train_X)
+        np.save(directory+"train_Y_"+str(max_seq_length)+".npy",train_Y)
+        np.save(directory+"test_X_"+str(max_seq_length)+".npy",test_X)
+        np.save(directory+"test_Y_"+str(max_seq_length)+".npy",test_Y)
+        del test_X
+        del test_Y
+    else:
+        train_X = np.load(directory+"train_X_"+str(max_seq_length)+".npy")
+        train_Y = np.load(directory+"train_Y_"+str(max_seq_length)+".npy")
     l_bert, model_ckpt = fetch_bert_layer()
     model = create_model(l_bert,model_ckpt)
     lr_scheduler = create_learning_rate_scheduler(max_learn_rate=1e-5,
                                                   end_learn_rate=1e-7,
                                                   warmup_epoch_count=20,
                                                   total_epoch_count=max_epochs)
-    model.fit(x=input_ids,y=label_ids,
+    model.fit(x=train_X,y=train_Y,
             validation_split=0.15,
             batch_size=48,
             shuffle=True,
