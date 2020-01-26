9a4550fdb93057a3f5d0e35b839480692a39eeb0
Atreya Shankar <shankar.atreya@gmail.com>
2020-01-26 17:50:17 +0100

    *shankar* new custom callback for f1 logging

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -6,6 +6,22 @@ import bert
 import math
 import numpy as np
 import tensorflow as tf
+import tensorflow.keras.backend as K
+
+def class_acc(label_threshold_less):
+    def argument_candidate_acc(y_true, y_pred):
+        class_id_true = K.cast(y_true,'int64')
+        class_id_preds = K.argmax(y_pred, axis=-1)
+        accuracy_mask = K.cast(K.less(class_id_preds,label_threshold_less),
+                               'float32')
+        accuracy_mask = 1 - accuracy_mask
+        class_acc_tensor = (K.cast(K.equal(class_id_true,
+                                          class_id_preds),
+                                   'float32') * accuracy_mask)
+        class_accuracy = (K.sum(class_acc_tensor)/
+                          K.maximum(K.sum(accuracy_mask),1))
+        return class_accuracy
+    return argument_candidate_acc
 
 def fetch_bert_layer():
     model_name = "albert_base_v2"
