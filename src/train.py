29c8d22150a8522c1bc5edf0e3cd52fc4c9ef6a6
Atreya Shankar <shankar.atreya@gmail.com>
2020-01-26 17:50:17 +0100

    *shankar* new custom callback for f1 logging

--- src/train.py
+++ src/train.py
@@ -64,9 +64,8 @@ def train(max_epochs=10,max_seq_length=128,
               shuffle=True,
               epochs=max_epochs,
               callbacks=[LrScheduler,
-                         FilteredF1Score(),
                          tf.keras.callbacks.EarlyStopping(monitor="val_loss",
-                                                          patience=20,
+                                                          patience=5,
                                                           restore_best_weights
                                                           =True)])
     model.save_weights('./test.h5', overwrite=True)
